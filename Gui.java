06176d58123fb13ec5c6d8accdf89d8ab2e58e35
mereska <91318074+mereska0@users.noreply.github.com>
2026-01-18 01:02:03 +0300

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -1,12 +1,29 @@
 package gui;
 
-//code will be down there..
+import javax.swing.*;
+import java.awt.*;
+import java.io.PrintWriter;
+
 public class Gui {
-    static private String name;
-    static private String input;
-
     public static void display() {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Chatter");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(900, 600);
+            frame.setLayout(new BorderLayout());
+            TextInput textInput = new TextInput();
+            frame.add(new JScrollPane(TextInput.textArea));
+            TextInput.setOnSendListener(e -> {
+                PrintWriter out = TextInput.getOut();
+                String message = e.getActionCommand();
+                String username = TextInput.getUserName();
+                System.out.println("CLIENT [" + username + "]: " + message);
+                if (out != null) {
+                    out.println("[" + username + "]: " + message);
+                }
+            });
 
+            frame.setVisible(true);
+        });
     }
-
 }
